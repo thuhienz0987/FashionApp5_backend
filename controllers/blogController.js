c849db5e14f07300b99d1e4d31f798bb100b4c53
DESKTOP-4FLCFBC\DELL <21521008@ms.uit.edu.vn>
2023-05-24 17:03:54 +0700

    Edit update collection, product, block

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,156 +1,164 @@
-const  Blog = require('../models/Blog');
-const Category = require('../models/Category');
-const Tag= require('../models/Tag');
-const cloudinary= require('../helper/imageUpload');
-const { url } = require('../helper/imageUpload');
-const NotFoundError = require('../errors/notFoundError');
+const Blog = require("../models/Blog");
+const Category = require("../models/Category");
+const Tag = require("../models/Tag");
+const cloudinary = require("../helper/imageUpload");
+const { url } = require("../helper/imageUpload");
+const NotFoundError = require("../errors/notFoundError");
 
+//upload array image use cloudinary
 
-//upload array image use cloudinary 
+const cloudinaryImageUploadMethod = async (file) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(file, (err, res) => {
+      if (err) return res.status(500).send("upload image error");
+      resolve({
+        res: res.secure_url,
+        public_id: res.public_id,
+      });
+    });
+  });
+};
 
-const cloudinaryImageUploadMethod = async file => {
-    return new Promise(resolve => {
-        cloudinary.uploader.upload( file , (err, res) => {
-          if (err) return res.status(500).send("upload image error")
-            resolve({
-              res: res.secure_url,
-              public_id: res.public_id,
-            }) 
-          }
-        ) 
-    })
+exports.postCreateProduct = async (req, res) => {
+  try {
+    const { title, detail, description, tag } = req.body;
+    let image = [];
+    const files = req.files;
+    for (const file of files) {
+      const { path } = file;
+      const newPath = await cloudinaryImageUploadMethod(path);
+      image.push({ url: newPath.res, public_id: newPath.public_id });
+    }
+    const blog = new Blog({
+      title,
+      detail,
+      description,
+      tag,
+      image: image,
+      posterImage: image[0],
+    });
+    await blog.save();
+    res.status(201).json({
+      message: "Blog created successfully",
+      blog: blog,
+    });
+  } catch (err) {
+    throw err;
   }
-
-exports.postCreateProduct =async(req,res)=>{
-    
-    try{
-        const {title, detail,description,tag}= req.body;
-        let image = [];
-        const files = req.files;
-        for (const file of files) {
-          const { path } = file;
-          const newPath = await cloudinaryImageUploadMethod(path);
-          image.push({url: newPath.res, public_id: newPath.public_id});
-        }
-        const blog = new Blog({
-            title,
-            detail,
-            description,
-            tag,
-            image: image,
-            posterImage: image[0],
-        });
-        await blog.save();
-        res.status(201).json({
-            message:'Blog created successfully',
-            blog: blog,
-        });
-    }
-    catch(err){
-        throw err;
-    }
 };
 
-exports.getBlogById= async(req,res)=>{
-    try{
-        const _id= req.params._id;
-        const blog= await Blog.findById(_id);
-        if(blog&&blog.isDeleted===false){
-            res.status(200).json(blog);
-        }
-        else if(blog&&blog.isDeleted===true){
-            res.status(410).send('Blog is deleted')
-        }
-        else{
-            throw new NotFoundError('Blog not found');
-        }
-
-    }catch(err){
-        throw err;
+exports.getBlogById = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const blog = await Blog.findById(_id);
+    if (blog && blog.isDeleted === false) {
+      res.status(200).json(blog);
+    } else if (blog && blog.isDeleted === true) {
+      res.status(410).send("Blog is deleted");
+    } else {
+      throw new NotFoundError("Blog not found");
     }
+  } catch (err) {
+    throw err;
+  }
 };
 
-exports.getAllBlog = async(req,res)=>{
-    Blog.find({isDeleted: false})
-    .then((result)=>{
-        res.send(result);
+exports.getAllBlog = async (req, res) => {
+  Blog.find({ isDeleted: false })
+    .then((result) => {
+      res.send(result);
     })
-    .catch((err)=>{
-        throw err;
-    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
-exports.getRandomBlog = async(req,res)=>{
-    const number = req.params.number
-    Blog.find({isDeleted: false}).limit(number)
-    .then((result)=>{
-        res.send(result);
+exports.getRandomBlog = async (req, res) => {
+  const number = req.params.number;
+  Blog.find({ isDeleted: false })
+    .limit(number)
+    .then((result) => {
+      res.send(result);
     })
-    .catch((err)=>{
-        throw err;
-    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
-exports.updateBlog= async(req,res)=>{
-    try{
-        const _id= req.params._id;
-        const {title, detail,description,tag,}= req.body;
-        let imageUpdate = [];
-        const files = req.files;
-        for (const file of files) {
-          const { path } = file;
-          const newPath = await cloudinaryImageUploadMethod(path);
-          imageUpdate.push({url: newPath.res, public_id: newPath.public_id});
-        }
-        const blog = await Blog.findById(_id);
-
-
-        let image =[]
-        for(let i=0;i< blog.image.length;i++){
-            image.push(blog.image[i].public_id);
-        }
-        //delete image from cloudinary
-        cloudinary.api.delete_resources(image,function(err,result){
-            console.log(result);
-        });
-
-        blog.title= title;
-        blog.detail=detail;
-        blog.description=description;
-        blog.tag=tag;
-        blog.image=imageUpdate;
-        blog.posterImage= imageUpdate[0];
-
-        const updateBlog = await blog.save();
-        res.status(200).json({
-            message: 'Blog updated',
-            blog: updateBlog,
-        });
+exports.updateBlog = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const { title, detail, description, tag, oldImage } = req.body;
+    let oldImageArray = [];
+    if (!Array.isArray(oldImage))
+        oldImageArray.push(oldImage)
+    else 
+        oldImageArray = oldImage
+    let imageUpdate = [];
+    const files = req.files;
+    for (const file of files) {
+      const { path } = file;
+      const newPath = await cloudinaryImageUploadMethod(path);
+      imageUpdate.push({ url: newPath.res, public_id: newPath.public_id });
     }
-    catch(err){
-        throw err;
+    const blog = await Blog.findById(_id);
+
+    let deleteImage = [];
+    console.log({oldImage})
+    for (let i = 0; i < blog.image.length; i++) {
+      const isNotDelete = oldImageArray.filter((element) => {
+        return (element === blog.image[i].public_id)
+      });
+      console.log(isNotDelete)
+      if (isNotDelete.length > 0) imageUpdate.push({url: blog.image[i].url, public_id: blog.image[i].public_id});
+      else deleteImage.push(blog.image[i].public_id);
     }
+    // //delete image from cloudinary
+    cloudinary.api.delete_resources(deleteImage, function (err, result) {
+      console.log(result);
+    });
+
+    blog.title = title;
+    blog.detail = detail;
+    blog.description = description;
+    blog.tag = tag;
+    blog.image = imageUpdate;
+    blog.posterImage = imageUpdate[0];
+
+    console.log({blog})
+    const updateBlog = await blog.save();
+    res.status(200).json({
+      message: "Blog updated",
+      blog: updateBlog,
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
-exports.deleteBlog= async(req,res)=>{
-    try{
-        const _id= req.params._id;
-        const blog = await Blog.findByIdAndUpdate({_id},{isDeleted: true},{new: true});
-        res.status(200).json({
-            message: 'Delete blog successfully',
-            blog: blog,
-        });    
-    }catch(err){
-        throw err;
-    }
+exports.deleteBlog = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const blog = await Blog.findByIdAndUpdate(
+      { _id },
+      { isDeleted: true },
+      { new: true }
+    );
+    res.status(200).json({
+      message: "Delete blog successfully",
+      blog: blog,
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
-exports.getDeletedBlog = async(req,res)=>{
-    Blog.find({isDeleted: true})
-    .then((result)=>{
-        res.send(result);
+exports.getDeletedBlog = async (req, res) => {
+  Blog.find({ isDeleted: true })
+    .then((result) => {
+      res.send(result);
     })
-    .catch((err)=>{
-        throw err;
-    })
+    .catch((err) => {
+      throw err;
+    });
 };
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,274 +1,301 @@
-const Product = require('../models/Product');
-const Category = require('../models/Category');
-const Tag= require('../models/Tag');
-const cloudinary = require('../helper/imageUpload');
-const NotFoundError = require('../errors/notFoundError');
-const { query } = require('express');
+const Product = require("../models/Product");
+const Category = require("../models/Category");
+const Tag = require("../models/Tag");
+const cloudinary = require("../helper/imageUpload");
+const NotFoundError = require("../errors/notFoundError");
+const { query } = require("express");
 
-const cloudinaryImageUploadMethod = async file => {
-    return new Promise(resolve => {
-        cloudinary.uploader.upload( file , (err, res) => {
-            if (err) return res.status(500).send("upload image error")
-            resolve({
-                res: res.secure_url,
-                public_id: res.public_id
-            }) 
-        }) 
-    })
-}
-
-exports.postCreateProduct = async (req,res)=>{
-    
-    try{
-    const {name,price,material,care,quantity,description,tag,categoryId} = req.body;
-
-        let image = [];
-        const files = req.files;
-        for (const file of files) {
-            const { path } = file;
-            const newPath = await cloudinaryImageUploadMethod(path);
-            image.push({url: newPath.res, public_id: newPath.public_id});
-        }
-
-        const product= await Product.create({
-
-            name,price,material,care,quantity,description,tag,categoryId,
-            image:image,
-            posterImage:image[0],
-        })
-        res.status(201).json({
-            message: 'Product created',
-            product: product,
-        });
-    }
-    catch(err){
-        throw err;
-    }
-},
-
-exports.updateProduct = async(req,res)=>{
-    try{
-        const _id = req.params._id;
-        const {name,price,material,care,description,tag,categoryId}= req.body;
-
-        let imageUpdate = [];
-        const files = req.files;
-        for (const file of files) {
-            const { path } = file;
-            const newPath = await cloudinaryImageUploadMethod(path);
-            imageUpdate.push({url: newPath.res, public_id: newPath.public_id});
-        }
-
-        const product = await Product.findById(_id);
-        let image =[]
-        for(let i=0;i< product.image.length;i++){
-            image.push(product.image[i].public_id);
-        }
-
-        await cloudinary.api.delete_resources(image,function(err,result){
-            console.log(result);
-        });
-
-        product.name =name;
-        product.price=price;
-        product.material=material;
-        product.care= care;
-        product.image=imageUpdate,
-        product.posterImage=imageUpdate[0],
-        product.description=description;
-        product.tag=tag;
-        product.categoryId=categoryId;
-        const updateProduct = await product.save();
-        res.status(200).json({
-            message: 'Product updated',
-            product: updateProduct,
-        });
-    }
-    catch(err){
-        throw err;
-    }
+const cloudinaryImageUploadMethod = async (file) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(file, (err, res) => {
+      if (err) return res.status(500).send("upload image error");
+      resolve({
+        res: res.secure_url,
+        public_id: res.public_id,
+      });
+    });
+  });
 };
 
-exports.deleteProduct =async(req,res)=>{
-    try{
-        const _id = req.params._id;
-        const product = await Product.findByIdAndUpdate({_id},{isDeleted: true}, {new: true});
+(exports.postCreateProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      price,
+      material,
+      care,
+      quantity,
+      description,
+      tag,
+      categoryId,
+    } = req.body;
 
-        res.status(200).json({
-            message: 'Delete product successfully',
-            product: product,
-        });    }
-    catch(err){
-        throw err;
+    let image = [];
+    const files = req.files;
+    for (const file of files) {
+      const { path } = file;
+      const newPath = await cloudinaryImageUploadMethod(path);
+      image.push({ url: newPath.res, public_id: newPath.public_id });
     }
-};
 
-exports.getAllProduct = async(req,res)=>{
-
-    //filtering
-    const queryObj ={...req.query};
-    const excludeFields = ["page","sort","limit","fields"];
-    excludeFields.forEach((el)=>delete queryObj[el]);
-    //
-    let queryStr= JSON.stringify(queryObj);
-    //gte= getter > ; lte = letter < ; 
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match)=>`$${match}`);
-    let query= Product.find(JSON.parse(queryStr),{isDeleted: false});
-
-    // sorting
-    if (req.query.sort) {
-        const sortBy= req.query.sort.split(",").join(" ");
-        query = query.sort(sortBy)
-      } else {
-        query = query.sort('-createdAt')
+    const product = await Product.create({
+      name,
+      price,
+      material,
+      care,
+      quantity,
+      description,
+      tag,
+      categoryId,
+      image: image,
+      posterImage: image[0],
+    });
+    res.status(201).json({
+      message: "Product created",
+      product: product,
+    });
+  } catch (err) {
+    throw err;
+  }
+}),
+  (exports.updateProduct = async (req, res) => {
+    try {
+      const _id = req.params._id;
+      const {
+        name,
+        price,
+        material,
+        care,
+        description,
+        tag,
+        categoryId,
+        oldImage,
+      } = req.body;
+      let oldImageArray = [];
+      if (!Array.isArray(oldImage)) oldImageArray.push(oldImage);
+      else oldImageArray = oldImage;
+      let imageUpdate = [];
+      const files = req.files;
+      for (const file of files) {
+        const { path } = file;
+        const newPath = await cloudinaryImageUploadMethod(path);
+        imageUpdate.push({ url: newPath.res, public_id: newPath.public_id });
       }
-   
-      //field limiting
-    //   if (req.query.fields) {
-    //     const fields = req.query.fields.split(',').join(' ')
-    //     query = query.select(fields)
-    //   } else {
-    //     query = query.select('-__v')
-    //   }
 
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit;
-    // query = query.skip(skip).limit(limit);
+      const product = await Product.findById(_id);
+      let deleteImage = [];
+      for (let i = 0; i < product.image.length; i++) {
+        const isNotDelete = oldImageArray.filter((element) => {
+          return element === product.image[i].public_id;
+        });
+        console.log(isNotDelete);
+        if (isNotDelete.length > 0)
+          imageUpdate.push({
+            url: product.image[i].url,
+            public_id: blog.image[i].public_id,
+          });
+        else deleteImage.push(product.image[i].public_id);
+      }
+      //
+      await cloudinary.api.delete_resources(deleteImage, function (err, result) {
+        console.log(result);
+      });
 
+      product.name = name;
+      product.price = price;
+      product.material = material;
+      product.care = care;
+      (product.image = imageUpdate),
+        (product.posterImage = imageUpdate[0]),
+        (product.description = description);
+      product.tag = tag;
+      product.categoryId = categoryId;
+      const updateProduct = await product.save();
+      res.status(200).json({
+        message: "Product updated",
+        product: updateProduct,
+      });
+    } catch (err) {
+      throw err;
+    }
+  });
 
-    // await Product.find(JSON.parse(queryStr),{isDeleted: false})
-    query
-    .then((result)=>{
-        console.log(result.length);
-        if (result.length === 0) throw new NotFoundError("No product found, please try again !"); 
-        res.status(200).send(result);
+exports.deleteProduct = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const product = await Product.findByIdAndUpdate(
+      { _id },
+      { isDeleted: true },
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Delete product successfully",
+      product: product,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.getAllProduct = async (req, res) => {
+  //filtering
+  const queryObj = { ...req.query };
+  const excludeFields = ["page", "sort", "limit", "fields"];
+  excludeFields.forEach((el) => delete queryObj[el]);
+  //
+  let queryStr = JSON.stringify(queryObj);
+  //gte= getter > ; lte = letter < ;
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  let query = Product.find(JSON.parse(queryStr), { isDeleted: false });
+
+  // sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  //field limiting
+  //   if (req.query.fields) {
+  //     const fields = req.query.fields.split(',').join(' ')
+  //     query = query.select(fields)
+  //   } else {
+  //     query = query.select('-__v')
+  //   }
+
+  // const page = req.query.page * 1 || 1;
+  // const limit = req.query.limit * 1 || 100;
+  // const skip = (page - 1) * limit;
+  // query = query.skip(skip).limit(limit);
+
+  // await Product.find(JSON.parse(queryStr),{isDeleted: false})
+  query
+    .then((result) => {
+      console.log(result.length);
+      if (result.length === 0)
+        throw new NotFoundError("No product found, please try again !");
+      res.status(200).send(result);
     })
-    .catch((err)=>{
-        throw err;
+    .catch((err) => {
+      throw err;
+    });
+};
+
+exports.getRandomProduct = async (req, res) => {
+  await Product.find({ isDeleted: false })
+    .limit(10)
+    .then((result) => {
+      res.send(result);
     })
+    .catch((err) => {
+      throw err;
+    });
 };
 
+exports.getProductById = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const product = await Product.findById(_id);
+    if (product && product.isDeleted === false) {
+      res.status(200).json(product);
+    } else if (product && product.isDeleted === true) {
+      res.status(410).send("Product is deleted");
+    } else {
+      throw new NotFoundError("Product not found");
+    }
+  } catch (err) {
+    throw err;
+  }
+};
 
-exports.getRandomProduct = async(req,res)=>{
- 
-    await Product.find({isDeleted : false}).limit(10)
-    .then((result)=>{
-        res.send(result);
+exports.getProductByCategoryId = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const category = Category.findById(_id);
+    if (!category)
+      throw new NotFoundError(
+        `The product with category _id ${_id} does not exists`
+      );
+    else if (category.isDeleted === true) {
+      res.status(410).send("Category is deleted");
+    } else {
+      const product = await Product.find({ categoryId: _id });
+      if (product.length === 0)
+        throw new NotFoundError(`Not found product in category id ${_id}`);
+
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.getProductByTagId = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const tag = await Tag.findById(_id);
+    if (!tag)
+      throw new NotFoundError(
+        `The product with tag _id ${_id} does not exists`
+      );
+    else if (tag.isDeleted === true) {
+      res.status(410).send("Tag is deleted");
+    } else {
+      const product = await Product.find({ tag: _id });
+      if (product.length === 0)
+        throw new NotFoundError(`Not found product in tag id ${_id}`);
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.getDeletedProduct = async (req, res) => {
+  Product.find({ isDeleted: true })
+    .then((result) => {
+      res.send(result);
     })
-    .catch((err)=>{
-        throw err;
-    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
-exports.getProductById = async(req,res)=>{
-    try{
-        
-        const _id = req.params._id;
-        const product = await Product.findById(_id);
-        if(product && product.isDeleted===false) {
-            res.status(200).json(product);
-        }else if(product && product.isDeleted===true){
-            res.status(410).send('Product is deleted');
-        }
-        else{
-            throw new NotFoundError('Product not found');
-        }
-    }
-    catch(err){
-        throw err;
-    }
+exports.getProductByMultipleTagId = async (req, res) => {
+  try {
+    const tags = req.query.tags;
+    const product = await Product.find({ tag: { $in: tags } });
+    if (product.length === 0) throw new NotFoundError(`Not found product`);
+    res.status(200).json(product);
+  } catch (err) {
+    throw err;
+  }
 };
 
-exports.getProductByCategoryId= async(req,res)=>{
-    try{
-        const _id= req.params._id;
-        const category = Category.findById(_id);
-        if (!category ) throw new NotFoundError(`The product with category _id ${_id} does not exists`);
-        else if(category.isDeleted===true) {
-            res.status(410).send('Category is deleted');
-        }
-        else {
-            const product = await Product.find({categoryId: _id});
-            if (product.length === 0) throw new NotFoundError(`Not found product in category id ${_id}`);
+exports.getNameTagByProductId = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    console.log(_id);
+    const product = await Product.findById(_id);
+    if (product && product.isDeleted === false) {
+      const tagIds = product.tag;
 
-        res.status(200).json(product);
-        }
-
-        
+      Tag.find({ _id: { $in: tagIds } })
+        .select("_id name")
+        .then((tags) => {
+          res.status(200).json(tags);
+        })
+        .catch((err) => {
+          throw err;
+        });
+    } else if (product && product.isDeleted === true) {
+      res.status(410).send("Product is deleted");
+    } else {
+      throw new NotFoundError("Product not found");
     }
-    catch(err){
-        throw err
-    }
+  } catch (err) {
+    throw err;
+  }
 };
-
-exports.getProductByTagId = async(req,res)=>{
-    try{
-        const _id = req.params._id;
-        const tag = await Tag.findById(_id);
-        if (!tag) throw new NotFoundError(`The product with tag _id ${_id} does not exists`);
-        else  if(tag.isDeleted===true) {
-            res.status(410).send('Tag is deleted');
-        }
-        else { 
-        const product = await Product.find({tag: _id});
-        if (product.length === 0) throw new NotFoundError(`Not found product in tag id ${_id}`);
-        res.status(200).json(product);
-        }
-
-    }
-    catch(err){
-        throw err;
-    }
-};
-
-exports.getDeletedProduct = async(req,res)=>{
-    Product.find({isDeleted: true})
-    .then((result)=>{
-        res.send(result);
-    })
-    .catch((err)=>{
-        throw err;
-    })
-};
-
-exports.getProductByMultipleTagId = async(req,res)=>{
-    try{
-        const tags = req.query.tags;
-        const product = await Product.find({ tag: { $in: tags } });
-        if (product.length === 0) throw new NotFoundError(`Not found product`);
-        res.status(200).json(product);
-    }
-    catch(err){
-        throw err;
-    }
-};
-
-
-exports.getNameTagByProductId = async(req,res)=>{
-        try{
-            
-            const _id = req.params._id;
-            console.log(_id);
-            const product = await Product.findById(_id);
-            if(product && product.isDeleted===false) {
-                const tagIds=product.tag;
-
-                Tag.find({_id: { $in: tagIds  }}).select('_id name')
-                    .then((tags) => {
-                        res.status(200).json(tags);
-                    })
-                    .catch((err) => {
-                        throw err;
-                    });
-            }else if(product && product.isDeleted===true){
-                res.status(410).send('Product is deleted');
-            }
-            else{
-                throw new NotFoundError('Product not found');
-            }
-        }
-        catch(err){
-            throw err;
-        }
-    };
