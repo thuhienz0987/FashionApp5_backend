871c23007a0672575881e4bd9f46caafa5fc3deb
DESKTOP-4FLCFBC\DELL <21521008@ms.uit.edu.vn>
2023-07-05 17:53:59 +0700

    adjust

--- routes/sizeRouters.js
+++ routes/sizeRouters.js
@@ -5,11 +5,11 @@ const verifyRoles = require('../middlewares/verifyRoles');
 
 const router = Router();
 
-router.get('/get-all-size', verifyRoles(ROLES_LIST.Admin), sizeController.getAllSize);
-router.get('/get-size-by-id/:_id',sizeController.getSizeById);
-router.post('/post-create-size',sizeController.postCreateSize);
-router.put('/put-update-size/:_id',sizeController.updateSize);
-router.delete('/delete-size/:_id',sizeController.deleteSize);
+router.get('/get-all-size', sizeController.getAllSize);
+router.get('/get-size-by-id/:_id', sizeController.getSizeById);
+router.post('/post-create-size', verifyRoles(ROLES_LIST.Admin), sizeController.postCreateSize);
+router.put('/put-update-size/:_id', verifyRoles(ROLES_LIST.Admin), sizeController.updateSize);
+router.delete('/delete-size/:_id', verifyRoles(ROLES_LIST.Admin), sizeController.deleteSize);
 router.get('/get-all-deleted-size', sizeController.getDeletedSize);
 
 module.exports= router;
